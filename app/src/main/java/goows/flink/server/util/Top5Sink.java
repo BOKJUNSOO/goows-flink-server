ea06331112a9979cb17fbf937ff8d3b63aac720c
복준수 <brianbok97@gmail.com>
2025-05-18 01:09:55 +0900

    Feat: 데이터 플로우 작성
    kafka 컨슈머 인스턴스 -> 데이터 처리 로직 동작 -> kafka 프로듀싱
    sink 까지의 단계와 프로듀싱 인스턴스의 close 동작까지 구현
    아마 수정될게 없어보임 (근데 왜 구독은 했는데 컨슈밍을 못하는지 모르겠따....)

--- app/src/main/java/goows/flink/server/util/Top5Sink.java
+++ app/src/main/java/goows/flink/server/util/Top5Sink.java
@@ -3,6 +3,10 @@ package goows.flink.server.util;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -10,10 +14,20 @@ import java.util.stream.Collectors;
 public class Top5Sink extends RichSinkFunction<Tuple2<String, Integer>> {
     private final Map<String, Integer> counts = new HashMap<>();
     private transient ObjectMapper mapper;
+    private transient KafkaProducer<String, String> producer;
 
+    // open 은 초기설정 메서로 사용됨
     @Override
     public void open(org.apache.flink.configuration.Configuration parameters) {
         mapper = new ObjectMapper();
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9093");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        
+        // kafka 프로듀서 인스턴스 생성
+        producer = new KafkaProducer<>(props);
     }
 
     @Override
@@ -21,6 +35,7 @@ public class Top5Sink extends RichSinkFunction<Tuple2<String, Integer>> {
         counts.put(value.f0, value.f1);
     }
 
+    // 실제 데이터 처리 로직 수행
     @Override
     public void close() throws Exception {
         List<Map<String, Object>> top5 = counts.entrySet().stream()
@@ -38,9 +53,19 @@ public class Top5Sink extends RichSinkFunction<Tuple2<String, Integer>> {
         message.put("timestamp", System.currentTimeMillis());
         message.put("topKeywords", top5);
 
-        // 추후에 kafka 로 메세지 발행
         String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(message);
-        System.out.println("=== Kafka Message ===");
-        System.out.println(json);
+
+        // producing message !_!
+        // top5-keywords 토픽으로 메세지 발행
+        ProducerRecord<String, String> record = new ProducerRecord<>("top5-keywords", json);
+        producer.send(record);
+
+        producer.flush();
+        if (producer != null) {
+            producer.close();
+        }
+//        System.out.println("=== Kafka Message ===");
+//        System.out.println(json);
+
     }
 }
