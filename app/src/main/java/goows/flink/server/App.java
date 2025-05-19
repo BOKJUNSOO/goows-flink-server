908a8e0076219808a36a3a250a6c5d9c6923cfbc
복준수 <brianbok97@gmail.com>
2025-05-19 23:34:23 +0900

    Feat: 파싱한 데이터 메세지에 추가
    description은 문자열처리
    user id 등 메타 데이터 처리

--- app/src/main/java/goows/flink/server/App.java
+++ app/src/main/java/goows/flink/server/App.java
@@ -1,13 +1,20 @@
 package goows.flink.server;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import goows.flink.server.kafka.KafkaSourceBuilder;
+import goows.flink.server.kafka.NewsMessage;
+import goows.flink.server.kafka.UserText;
 import goows.flink.server.util.KomoranProcessor;
 import goows.flink.server.util.Top5Sink;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 
+
 public class App {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
     public static void main(String[] args) throws Exception {
 
         // flink application entry point !_!
@@ -15,10 +22,30 @@ public class App {
 
         // 카프카 토픽으로부터 데이터 구독
         DataStream<String> input = env.addSource(KafkaSourceBuilder.create("news-search-topic"));
-        input.print();
+
+
+        // 키워드, userId 파싱
+        DataStream<NewsMessage> parsed = input
+                .map(json -> new ObjectMapper()
+                        .readValue(json, NewsMessage.class)
+                );
+
+        parsed.toString();
+
+        // newsList 필드만 JSON 문자열로 변환
+        DataStream<UserText> enrichedDescriptions = parsed.map(dto -> {
+            String descriptionsJson;
+            try {
+                descriptionsJson = mapper.writeValueAsString(dto.getNewsList());
+            } catch (Exception e) {
+                descriptionsJson = "[]";
+            }
+            return new UserText(dto.getMemberId(), descriptionsJson);
+        });
+
 
         // one-off
-        input
+        enrichedDescriptions
                 .map(new KomoranProcessor())
                 .addSink(new Top5Sink());
 
